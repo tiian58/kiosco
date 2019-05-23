e70a048e2dbed7824e9c86fd4bbd3e059c1d895e
tiian58 <tiian58@gmail.com>
2019-05-23 00:55:25 -0300

    first commit

---
+++ src/main/java/kioscofast/kiosco/KioscoApplication.java
@@ -0,0 +1,14 @@
+package kioscofast.kiosco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KioscoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KioscoApplication.class, args);
+    }
+    
+
+}
---
+++ src/test/java/kioscofast/kiosco/KioscoApplicationTests.java
@@ -0,0 +1,16 @@
+package kioscofast.kiosco;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KioscoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
